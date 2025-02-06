3d8d55e2eafeec2c17c6ae7b972eb5fe221b47fb
Noah Davis <11672+noahd1@users.noreply.github.com>
2025-02-05 16:56:02 -0800

    Add Semaphore as supported CI provider for coverage uploads

--- qlty-coverage/src/ci.rs
+++ qlty-coverage/src/ci.rs
@@ -2,11 +2,13 @@ mod circleci;
 mod codefresh;
 mod github;
 mod gitlab;
+mod semaphore;
 
 pub use circleci::CircleCI;
 pub use codefresh::Codefresh;
 pub use github::GitHub;
 pub use gitlab::GitLab;
+pub use semaphore::Semaphore;
 use qlty_types::tests::v1::CoverageMetadata;
 
 pub trait CI {
@@ -63,5 +65,6 @@ pub fn all() -> Vec<Box<dyn CI>> {
         Box::<GitLab>::default(),
         Box::<CircleCI>::default(),
         Box::<Codefresh>::default(),
+        Box::<Semaphore>::default(),
     ]
 }
