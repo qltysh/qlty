64f1ee5895e2069273d766ac617a2428604306bf
marschattha <arslan_21@rocketmail.com>
2025-06-24 22:50:14 +0500

    feat: make strip prefix coverage transformer optional

--- qlty-coverage/src/publish/planner.rs
+++ qlty-coverage/src/publish/planner.rs
@@ -197,7 +197,9 @@ impl Planner {
         if let Some(prefix) = self.settings.strip_prefix.clone() {
             transformers.push(Box::new(StripPrefix::new(prefix)));
         } else {
-            transformers.push(Box::new(StripPrefix::new_from_git_root()?));
+            if let Ok(strip_prefix) = StripPrefix::new_from_git_root() {
+                transformers.push(Box::new(strip_prefix));
+            }
         }
 
         transformers.push(Box::new(StripDotSlashPrefix));
